983059cc28c07816420fd42e1b9a0eac111c881c
Sulmaan-M <farouq2.mahmood@live.uwe.ac.uk>
2026-04-23 18:24:31 +0100

    WaveformSample struct creation

---
+++ waveform.h
@@ -0,0 +1,11 @@
+typedef struct {
+
+    double timestamp;
+    double phase_A_voltage;
+    double phase_B_voltage;
+    double phase_C_voltage;
+    double line_current;
+    double frequency;
+    double power_factor;
+    double thd_percent;
+} WaveformSample;
