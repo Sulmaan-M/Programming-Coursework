c72203a5cfefc7efaa71a8cdc9948cc329bdd4f2
Sulmaan-M <farouq2.mahmood@live.uwe.ac.uk>
2026-04-23 18:42:49 +0100

    file opening test

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main () {
+
+    FILE* file = fopen ("power_quality_log.csv", "r");
+
+    if (file == NULL) {
+        printf("Error: Could not open file\n");
+        return 1;
+    }
+
+    printf("Success file was found\n");
+
+    fclose(file);
+
     return 0;
 }
