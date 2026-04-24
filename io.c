c3732cbceec7aeb66601437551a15ec8855d5c15
Sulmaan-M <farouq2.mahmood@live.uwe.ac.uk>
2026-04-24 01:56:55 +0100

    use of malloc with row count to show size of memory allocation and total memory needed

--- io.c
+++ io.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include "io.h"
+#include <stdlib.h>
+#include "waveform.h"
 
-int load_csv_file(char* filename) {
-    FILE* file = fopen(filename, "r");
-
+WaveformSample* load_csv_file(char* filename, int* total_rowcount) {
+    FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error: Could not open file\n");
-        return 1; //error code if file not found
+        return NULL;
     }
 
     char row_counter[1024];
     int count = 0;
-
-
-    if (fgets(row_counter, sizeof (row_counter), file) !=NULL) {
-        printf("Header row ignored successfully.\n");
+    fgets(row_counter, sizeof(row_counter), file);
+    while (fgets(row_counter, sizeof(row_counter), file) != NULL) {
+        count++;
     }
+    *total_rowcount = count;
 
-    while (fgets(row_counter, sizeof(row_counter), file) !=NULL){
-        count++; //increase row count by 1
+    size_t row_size = sizeof(WaveformSample);
+    size_t total_memory = count * row_size;
+    WaveformSample *memory_allocation = (WaveformSample*) malloc (total_memory);
+
+
+    if (memory_allocation == NULL) {
+        printf("Error: memory not allocated\n");
+        fclose(file);
+        return NULL;
     }
 
     printf("Total number of rows found: %d rows of data.\n", count);
+    printf("Size of one WaveformSample: %zu bytes\n", row_size);
+    printf("Total memory to allocate: %zu bytes\n", total_memory);
 
-fclose(file);
+    fclose(file);
 
-    return count;
+    return memory_allocation;
 }
--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h> // FIX: This is needed for free()
 #include "io.h"
 
 int main() {
 
- int load_data = load_csv_file("power_quality_log.csv");
+    int final_row_count = 0;
+    char* filename = "power_quality_log.csv";
+
+    WaveformSample* data_array = load_csv_file(filename, &final_row_count);
+
+        free(data_array);
 
     return 0;
 }
