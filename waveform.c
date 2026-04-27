a3a07243dc0ac667de3ff6f27473c9af32c94f24
Sulmaan-M <farouq2.mahmood@live.uwe.ac.uk>
2026-04-27 15:06:03 +0100

    upgrade to each function to assign three values to results pointer and function for tolernance to print directly to main.c

--- waveform.c
+++ waveform.c
@@ -2,68 +2,83 @@
 #include <math.h>
 #include "waveform.h"
 
-double calculate_rms_voltage(WaveformSample *data, int rows) {
+void calculate_rms_voltage(WaveformSample *data, int rows, double *results) {
 
     double sum_of_squares_Phase_A = 0.0;
+    double sum_of_squares_Phase_B = 0.0;
+    double sum_of_squares_Phase_C = 0.0;
 
     for (int i = 0; i < rows; i++) {
         double current_voltage_Phase_A = data[i].phase_A_voltage;
+        double current_voltage_Phase_B = data[i].phase_B_voltage;
+        double current_voltage_Phase_C = data[i].phase_C_voltage;
 
         sum_of_squares_Phase_A += (current_voltage_Phase_A * current_voltage_Phase_A);
+        sum_of_squares_Phase_B += (current_voltage_Phase_B * current_voltage_Phase_B);
+        sum_of_squares_Phase_C += (current_voltage_Phase_C * current_voltage_Phase_C);
     }
 
-    double mean_square = sum_of_squares_Phase_A / rows;
-    return sqrt(mean_square);
-
+    results[0] = sqrt(sum_of_squares_Phase_A / rows);
+    results[1] = sqrt(sum_of_squares_Phase_B / rows);
+    results[2] = sqrt(sum_of_squares_Phase_C / rows);
 }
 
-double calculate_peak_to_peak (WaveformSample *data, int rows) {
+void calculate_peak_to_peak(WaveformSample *data, int rows, double *results) {
 
-    double max_voltage = data [0].phase_A_voltage;
-    double min_voltage = data [0].phase_A_voltage;
-
-    for (int i = 0; i < rows; i++){
-
-        double current_voltage = data [i].phase_A_voltage;
-
-        if (current_voltage > max_voltage) {
-            max_voltage = current_voltage;
-
-        }
-
-        if (current_voltage < min_voltage) {
-           min_voltage = current_voltage;
-}
-    }
-         return (max_voltage - min_voltage);
-}
-
-double calculate_dc_offset(WaveformSample *data, int rows) {
-
-    double sum = 0.0;
+    double max_A = data[0].phase_A_voltage, min_A = data[0].phase_A_voltage;
+    double max_B = data[0].phase_B_voltage, min_B = data[0].phase_B_voltage;
+    double max_C = data[0].phase_C_voltage, min_C = data[0].phase_C_voltage;
 
     for (int i = 0; i < rows; i++) {
-        sum += data[i].phase_A_voltage;
+
+        if (data[i].phase_A_voltage > max_A) max_A = data[i].phase_A_voltage;
+        if (data[i].phase_A_voltage < min_A) min_A = data[i].phase_A_voltage;
+
+        if (data[i].phase_B_voltage > max_B) max_B = data[i].phase_B_voltage;
+        if (data[i].phase_B_voltage < min_B) min_B = data[i].phase_B_voltage;
+
+        if (data[i].phase_C_voltage > max_C) max_C = data[i].phase_C_voltage;
+        if (data[i].phase_C_voltage < min_C) min_C = data[i].phase_C_voltage;
     }
-    return sum / (double) rows;
+
+    results[0] = max_A - min_A;
+    results[1] = max_B - min_B;
+    results[2] = max_C - min_C;
 }
 
-int detect_clipping(WaveformSample *data, int rows) {
-    int count = 0;
+void calculate_dc_offset(WaveformSample *data, int rows, double *results) {
+    double sum_A = 0.0, sum_B = 0.0, sum_C = 0.0;
 
     for (int i = 0; i < rows; i++) {
-        if (fabs(data[i].phase_A_voltage)>=324.9) {
-            count++;
+        sum_A += data[i].phase_A_voltage;
+        sum_B += data[i].phase_B_voltage;
+        sum_C += data[i].phase_C_voltage;
+    }
+
+    results[0] = sum_A / rows;
+    results[1] = sum_B / rows;
+    results[2] = sum_C / rows;
+}
+
+void detect_clipping(WaveformSample *data, int rows, int *results) {
+
+    results[0] = 0;
+    results[1] = 0;
+    results[2] = 0;
+
+    for (int i = 0; i < rows; i++) {
+        if (fabs(data[i].phase_A_voltage) >= 324.9) results[0]++;
+        if (fabs(data[i].phase_B_voltage) >= 324.9) results[1]++;
+        if (fabs(data[i].phase_C_voltage) >= 324.9) results[2]++;
+    }
+}
+
+    void check_tolerance(double rms_voltage) {
+        if (rms_voltage >= 207.0 && rms_voltage <= 253.0) {
+
+            printf("PASS (Within 10%% of 230V)");
+        }
+        else {
+            printf("FAIL (Out of tolerance)");
         }
     }
-
-    return count;
-}
-
-int check_tolerance(double rms_voltage) {
-
-    if (rms_voltage >= 207.0 && rms_voltage <= 253.0) {
-        return 1;
-    }
-     return 0;
-}
