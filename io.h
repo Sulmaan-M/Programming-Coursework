eca3e95f378b7f6c2795d8910c12cee9506c2755
Sulmaan-M <farouq2.mahmood@live.uwe.ac.uk>
2026-04-27 15:46:15 +0100

    export to file function created in headerfile waveform.h with pointers assingments to results

--- io.h
+++ io.h
@@ -2,6 +2,8 @@
 #define io_h
 #include "waveform.h"
 
-WaveformSample* load_csv_file(char* filename, int* total_rowcount);
+WaveformSample* load_csv_file(const char* filename, int* total_rowcount);
+
+void export_results_to_file(const char* filename, double* rms, double* ptp, double* dc, int* clip);
 
 #endif
